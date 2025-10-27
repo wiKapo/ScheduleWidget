86442b8cf31a26db255d6715805e80452713a353
wiKapo <71190167+wiKapo@users.noreply.github.com>
2025-10-28 00:39:32 +0100

    Próbowanie użycia kalendarza

--- app/src/main/java/com/wikapo/schedulewidget/ScheduleWidget.kt
+++ app/src/main/java/com/wikapo/schedulewidget/ScheduleWidget.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.unit.dp
 import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
-import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
@@ -20,6 +19,7 @@ import androidx.glance.text.Text
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.net.URL
+import java.util.Calendar
 import javax.net.ssl.HttpsURLConnection
 
 
@@ -27,21 +27,25 @@ class ScheduleWidgetReciever : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = ScheduleWidget()
 }
 
+val calendar = Calendar.getInstance()
+
 class ScheduleWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         fetchSchedule()
 
+        update(context, id)
+
         provideContent {
-            ScheduleContent()
+            ScheduleContent(context, id)
         }
     }
 }
 
 var result = "LOL"
 
-suspend fun fetchSchedule() {
-    get("https://eti.thefen.me/schedule")
+suspend fun fetchSchedule(date: String = "") {
+    get("https://eti.thefen.me/schedule/$date")
 }
 
 suspend fun get(url: String) = withContext(Dispatchers.IO) {
@@ -56,24 +60,39 @@ suspend fun get(url: String) = withContext(Dispatchers.IO) {
     }
 }
 
+//Log.d("calendar","${calendar.get(Calendar.YEAR)}-${calendar.get(Calendar.MONTH) + 1}-${calendar.get(Calendar.DATE)})"
 @Composable
-fun ScheduleContent() {
+fun ScheduleContent(context: Context, id: GlanceId) {
     Column(
         modifier = GlanceModifier.fillMaxSize(),
         verticalAlignment = Alignment.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Where to?", modifier = GlanceModifier.padding(12.dp))
-        Row(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Button(
-                text = "Home",
-                onClick = actionStartActivity<MainActivity>()
+                text = "<=",
+                onClick = {
+                    calendar.roll(Calendar.DATE, false)
+//                    ScheduleWidget().update(context, id)
+                }
+            )
+            Text(
+                text = "${calendar.get(Calendar.DATE)}.${calendar.get(Calendar.MONTH) + 1}." +
+                        "${calendar.get(Calendar.YEAR)}",
+                modifier = GlanceModifier.padding(6.dp)
             )
             Button(
-                text = "Work",
-                onClick = actionStartActivity<MainActivity>()
+                text = "=>",
+                onClick = { calendar.roll(Calendar.DATE, false) }
             )
         }
         Text(result, modifier = GlanceModifier.padding(24.dp))
+        Button(
+            text = "Reset",
+            onClick = {}
+        )
     }
 }
